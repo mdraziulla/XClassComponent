7cf45a1fe16221d8896e30a5877efcf40df00cf8
Md Razi Ullah <mdraziullah631@gmail.com>
2024-03-29 21:33:50 +0530

    Add Class Component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import CounterApp from './components/CounterApp'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <CounterApp />
+      </div>
+    </>
+  )
+}
+
+export default App
